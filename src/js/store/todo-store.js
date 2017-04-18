a9b5578c40620ab4b4fdaae393e433f95ba87a38
Brian Sweeney <bsweeney930@gmail.com>
2017-04-18 16:55:15 -0400

    DRYed out to-doStore

--- src/js/store/todo-store.js
+++ src/js/store/todo-store.js
@@ -1,22 +1,28 @@
 import dispatcher from '../utils/dispatcher';
 
+let ping = function() {
+  dispatcher.emit('todo-store:change', todoStore);
+}
+
 let todoStore = {
   items: []
 };
 
 let addItem = (todoText) => {
   todoStore.items.push({text: todoText, complete: false});
-  dispatcher.emit('todo-store:change', todoStore);
+  ping();
 };
 
 let deleteItem = (index) => {
   todoStore.items.splice(index, 1);
-  dispatcher.emit('todo-store:change', todoStore)
+  ping()
 };
 
 let checkItem = (index) => {
-  // To be implemented
-  console.log(todoStore.items[index]);
+  console.log(todoStore.items[index])
+  todoStore.items[index].complete = true;
+  console.log(todoStore.items[index])
+  ping()
 }
 
 dispatcher.on('todo:user-added', addItem);
